b05bd8e934d8293c4db7a47fd68306e5f04870f4
Annette Hawks <hawksannette@gmail.com>
2025-07-02 20:23:46 -0600

    Steps 2 and 3

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
-﻿//Annette Hawks 
+﻿//1. Comment
+//Annette Hawks 
 //7/2/2025
 //CS 1405 Pig Latin/ Encoder
+// 2. Greeting
+        Console.WriteLine("Pig Latin Converter and Encoder");
+
+        // 3. Collect a string.
+        Console.Write("Please enter a message: ");
+        string message = Console.ReadLine();
