5b4a73de484abfcf23fa265d4df0c3baa6a43c16
Annette Hawks <hawksannette@gmail.com>
2025-07-02 21:02:14 -0600

    Pig Latin Method

--- Program.cs
+++ Program.cs
@@ -8,3 +8,33 @@
         // 3. Collect a string.
         Console.Write("Please enter a message: ");
         string message = Console.ReadLine();
+        
+
+
+
+
+
+        
+// Pig Latin Method
+static string PigLatin(string word)
+{
+    string vowels = "aeiou";
+    word = word.ToLower(); // make everything lowercase so I don't have to worry about capitalization. 
+
+    int index = 0;
+
+    // Find the index of the first vowel
+    while (index < word.Length && !vowels.Contains(word[index]))
+    {
+        index++;
+    }
+
+    string pigLatinWord;
+
+    if (index == 0)
+        pigLatinWord = word + "way";      // Starts with a vowel
+    else
+        pigLatinWord = word.Substring(index) + word.Substring(0, index) + "ay"; // Consonant start
+
+    return pigLatinWord;
+}
