39838c9602755c699ecba58e6bb61a6615ebaa5a
Annette Hawks <hawksannette@gmail.com>
2025-07-02 21:12:54 -0600

    Step 6 and Encoder Method

--- Program.cs
+++ Program.cs
@@ -21,10 +21,15 @@ string pigLatin = string.Join(" ", words);
 // 5. Print the phrase in Pig Latin
 Console.WriteLine($"\nPig latin: {pigLatin}");
 
+// 6. Get an offset (set to 3)
+int shift = 3;
+string encoded = EncodeShift(pigLatin, shift);
+Console.WriteLine($"\nEncoded (shift {shift}): {encoded}");
+
 // Pig Latin Method
 static string PigLatin(string word)
 {
-    string vowels = "aeiou";       
+    string vowels = "aeiou";
     word = word.ToLower(); // make everything lowercase so I don't have to worry about capitalization. 
 
     int index = 0;
@@ -43,4 +48,24 @@ static string PigLatin(string word)
         pigLatinWord = word.Substring(index) + word.Substring(0, index) + "ay"; // Consonant start
 
     return pigLatinWord;
+}
+//Encoder Method
+static string EncodeShift(string message, int shift)
+{
+    string result = "";
+
+    foreach (char letter in message)
+    {
+        if (char.IsLetter(letter))
+        {
+            char shifted = (char)((((letter - 'a') + shift) % 26) + 'a');
+            result += shifted;
+        }
+        else
+        {
+            result += letter;
+        }
+    }
+
+    return result;
 }
